2c2d4af42d846969a9518f950f142e99d29818b2
Abhishek Anand <abhishek.664128@gmail.com>
2026-03-06 19:16:25 +0530

    Solved Remove Duplicate array using two pointer

---
+++ 02-Array/src/RemoveDuplicate.java
@@ -0,0 +1,25 @@
+public class RemoveDuplicate {
+
+    public static int removeDupli(int [] num){
+        int i=0;
+        for (int j=1;j<num.length;j++){
+            if (num[i] != num[j]){
+                i++;
+                num[i] =num[j];
+            }
+        }
+        return i+1;
+    }
+    public static void main(String []args){
+        int[] num = {0,0,1,1,1,2,2,3,4};
+//        int [] num ={0,1,1,2};
+
+        int k = removeDupli(num);
+
+        System.out.println("Unique count: " + k);
+
+        for(int i = 0; i < k; i++){
+            System.out.print(num[i] + " ");
+        }
+    }
+}
